af3c95d40906d10fdc7ca043150050becc0efa42
Gor0o0 <moasobakaposholvon@gmail.com>
2025-12-24 19:00:03 +0300

    765

--- app/main.py
+++ app/main.py
@@ -2,21 +2,18 @@ from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
-
 from app.database import Base, engine, SessionLocal
-from app.models import Song, Playlist   # ✅ импортируем обе модели
+from app.models import Song, Playlist
+from fastapi.staticfiles import StaticFiles
 
-# Создаём таблицы (если их ещё нет)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 Base.metadata.create_all(bind=engine)
 
-# Инициализация FastAPI
 app = FastAPI()
 
-# Подключение шаблонов Jinja2
 templates = Jinja2Templates(directory="templates")
 
-
-# Функция для получения сессии БД
 def get_db():
     db = SessionLocal()
     try:
@@ -24,12 +21,10 @@ def get_db():
     finally:
         db.close()
 
-
-# Главная страница — вывод песен и плейлистов из базы
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request, db: Session = Depends(get_db)):
-    songs = db.query(Song).all()         # ✅ список песен
-    playlists = db.query(Playlist).all() # ✅ список плейлистов
+    songs = db.query(Song).all()
+    playlists = db.query(Playlist).all()
     return templates.TemplateResponse("index.html", {
         "request": request,
         "songs": songs,
